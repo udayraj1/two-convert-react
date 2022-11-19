f9db430cc2df8d42b6d1a65703e36ec06059fc67
udayraj1 <udayrajsawant3@gmail.com>
2022-11-19 18:03:21 +0530

    Committing the code with some documentation

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,15 @@
 import React from "react"
 
-const Card = ({title="Basketball",subText="A default subtext",buttonText="MyButton", sourceImage="https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}) => {
+//here we used title sub text, button text, and sourceImage as a default if in cardSection developer only uses our component as <Card /> then this default attributes gets added to that component
+const Card = ({title="A default title",subText="A default subtext",buttonText="A default Button", sourceImage="https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}) => {
     return(
             <div className="card" style={{width: "18rem"}}>
-              <img
+              <img //here in src SourceImage will get's added either a default one which is mentioned above or the one dev insert in respective <Card /> component in cardsection.js same goes for subText, title and buttonText mentioned below i.e. for tags p, h5, a
                 src={sourceImage}
                 className="card-img-top"
                 alt="..."
               />
-              <div className="card-body">
+              <div className="card-body"> 
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{subText}</p>
                 <a href="#" className="btn btn-success">{buttonText}</a>
--- src/CardSection.js
+++ src/CardSection.js
@@ -8,14 +8,16 @@ const CardSection = () => {
           We love new friends!
         </h2>
         <div className="row">
-          <div className="col-4">
-            <Card title="Learncodeonline" sourceImage="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" subText="A online learning platform for the developers with pre recorded courses" buttonText="My LCO App"/>
+          <div className="col-4"> 
+          {/* Here we are mentioning the title sourceImage subtext and buttonText so for this component it's going to pick these attributes over the default ones which are there in Card.js */}
+            <Card title="Learncodeonline" sourceImage="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" subText="A online learning platform for the developers with pre recorded courses" buttonText="My LCO App"/> 
           </div>
           <div className="col-4">
           <Card title="ineuron.ai" sourceImage="https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" subText="A online learning platform for the developers with live course sessions" buttonText="iNeuron App"/>
           </div>
           <div className="col-4">
-          <Card title="myApp" sourceImage="https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" subText="A online learning platform for the developers with live course sessions" buttonText="my personal App"/>
+            {/* Here we haven't mentioned any title, sourceImage, subText and buttonText properties so for this component these attributes are going to be picked from the Card.js which are default */}
+          <Card />
           </div>
         </div>
       </div>
